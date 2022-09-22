2a6d751bd28ba1744e46c8d2cc50e7c51c3d9b4d
cosmonet-fr <stephane.billois@hotmail.com>
2022-09-22 17:15:58 +0200

    000

--- gui.py
+++ gui.py
@@ -5,6 +5,7 @@ import functions as function
 #import gui_functions as gui_function
 
 def runTest(button_1):
+    print(csvFile)
     if (titleCheck.get_active() == True):
         function.title()
     if (metaCheck.get_active() == True):
@@ -20,9 +21,12 @@ mainLabel = Gtk.Label()
 mainLabel.set_markup('<span weight="bold" font="roboto">Sélectionnez les tests à effectuer : </span>')
 titleCheck = Gtk.Switch()
 titleLabel = Gtk.Label('Tester les balises <title> ')
+titleLabel.set_justify(Gtk.Justification.LEFT) # Ce truc ne fonctionne pas !!!
 titleCheck.set_active(True)
 metaCheck = Gtk.Switch()
 metaLabel = Gtk.Label('Tester les metas descriptions')
+csvFile = Gtk.FileChooser
+csvUploadButton = Gtk.FileChooserButton(title="Fichier CSV")
 button_1 = Gtk.Button(label='Valider !')
 button_1.connect('clicked', runTest)
 
@@ -31,7 +35,8 @@ grid.attach(titleCheck, 6,1,1,1)
 grid.attach(titleLabel, 0,1,1,1)
 grid.attach(metaCheck, 6,2,1,1)
 grid.attach(metaLabel, 0,2,1,1)
-grid.attach(button_1, 0, 6, 6, 1)
+grid.attach(csvUploadButton, 0, 6, 6, 1)
+grid.attach(button_1, 0, 7, 6, 1)
 grid.set_row_spacing(8)
 
 mainWindow.add(grid)
